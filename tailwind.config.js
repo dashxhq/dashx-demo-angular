af24d5e7bacf0aec6bc47a24ea035ef113a1a2f6
NagajyothiPrakash <nagajyothi@keepworks.com>
2022-07-08 23:50:47 +0530

    Upgrade angular cli

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
   plugins: [],
-}
+};
