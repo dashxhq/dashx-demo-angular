1cda25edc5c09b05d7a91eeea0be67165e34da54
“NagajyothiPrakash” <nagajyothi@keepworks.com>
2022-07-19 22:44:57 +0530

    Add sidebar and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { SharedModule } from './shared/shared.module';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     SharedModule,
     HomeModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,11 @@ import { FormHeaderComponent } from './components/form-header/form-header.compon
 import { AlertBoxComponent } from './components/alert-box/alert-box.component';
 import { SuccessBoxComponent } from './components/success-box/success-box.component';
 import { LoaderComponent } from './components/loader/loader.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { menuAlt2, search, bell, HeroIconModule } from 'ng-heroicon';
+import { MatMenuModule } from '@angular/material/menu';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -11,13 +16,35 @@ import { LoaderComponent } from './components/loader/loader.component';
     AlertBoxComponent,
     SuccessBoxComponent,
     LoaderComponent,
+    NavbarComponent,
+    SidebarComponent,
   ],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    HeroIconModule.withIcons(
+      {
+        menuAlt2,
+        search,
+        bell,
+      },
+      {
+        /**
+         * Child level options.
+         * Non passed options will use the rootModule options.
+         */
+        defaultHostDisplay: 'block',
+      }
+    ),
+    MatMenuModule,
+    MatSidenavModule,
+  ],
   exports: [
     AlertBoxComponent,
     FormHeaderComponent,
     SuccessBoxComponent,
     LoaderComponent,
+    NavbarComponent,
+    SidebarComponent,
   ],
 })
 export class SharedModule {}
